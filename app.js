b17a2034f4ee137bd3ab3cd91abf2688450a4274
Amanpreet Singh <55251373+a493sing@users.noreply.github.com>
2020-02-23 17:03:36 -0500

    Edited app.js file

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function(){
+//app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(3000, process.env.IP, function(){
    console.log("The EMS Server Has Started!");
 });
