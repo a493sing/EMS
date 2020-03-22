368d1b84e372aaaa3a395da89004d97041daaba0
Amanpreet Singh <55251373+a493sing@users.noreply.github.com>
2020-03-22 19:43:54 -0400

    Update decorations.js

--- models/decorations.js
+++ models/decorations.js
@@ -1,12 +1,19 @@
 var mongoose = require("mongoose");
 
 var decorationsSchema = new mongoose.Schema({
-   name: String,
+
+   name: {type: String, required: true},
+
    image: String,
    description: String,
    location: String,
    price: String,
-   contactno: String
+
+   contactno: String,
+   id: {
+      type: mongoose.Schema.Types.ObjectId
+   }
+
 });
 
 module.exports = mongoose.model("decorations", decorationsSchema);
