9c4439be5b30782b80482a33e1f369570776052e
Amanpreet Singh <55251373+a493sing@users.noreply.github.com>
2020-03-22 19:43:15 -0400

    Update catering.js

--- models/catering.js
+++ models/catering.js
@@ -1,13 +1,17 @@
 var mongoose = require("mongoose");
 
 var cateringSchema = new mongoose.Schema({
-   name: String,
+   name: {type: String, required: true},
    image: String,
    description: String,
    location: String,
    price: String,
-   beverages: Boolean,
-   contactno: String
+
+   beverages: String,
+   contactno: String,
+   id: {
+      type: mongoose.Schema.Types.ObjectId
+   }
 });
 
 module.exports = mongoose.model("catering", cateringSchema);
