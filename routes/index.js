aef944ffd6b34bf7c7adf312f2eaf33ac6e85f4b
Amanpreet Singh <55251373+a493sing@users.noreply.github.com>
2020-03-22 19:44:56 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ var express = require("express");
 var router  = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
-var Venues = require("../models/venues");
 var Catering = require("../models/catering");
-var Decorations  = require("../models/decorations");
+var Venues = require("../models/venues");
+var Decorations = require("../models/decorations");
 
 //root route
 router.get("/", function(req, res){
@@ -12,41 +12,15 @@ router.get("/", function(req, res){
 });
 
 // show register form
-router.get("/register", function(req, res){
-   res.render("register"); 
-});
 
-router.get('/decorations', function(req, res) {
-    Decorations.find(function(err, decorations) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render('decorations', { decorations: decorations });
-            //console.log(venues);
-        }
-    });
-});
+router.get("/register", function(req, res, next){
+   try {
+   res.header('token', JSON.stringify({ token: 'token' })); 
+   res.render("register");
+   } catch(err) {
+       next(err);
+   }
 
-router.get('/venues', function(req, res) {
-    Venues.find(function(err, venues) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render('venues', { venues: venues });
-            //console.log(venues);
-        }
-    });
-});
-
- router.get('/catering', function(req, res) {
-    Catering.find(function(err, catering) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render('catering', { catering: catering });
-            //console.log(venues);
-        }
-    });
 });
 
 
@@ -88,6 +62,180 @@ router.get("/logout", function(req, res){
    req.flash("success", "Successfully LOGGED OUT!");
    res.redirect("/");
 });
+
+
+router.get("/venues/:id", function(req, res){
+    //find the venues with provided ID
+    Venues.findById(req.params.id, function(err, ven){
+        if(err){
+            console.log(err);
+            console.log("Testing");
+        } else {
+            console.log("Listing all venues....")
+
+            //render show template with that venues
+            res.render("venues/showvenue", {venue: ven});
+        }
+    });
+});
+
+
+//catering
+router.get("/catering/:id", function(req, res){
+    //find the catering with provided ID
+    Catering.findById(req.params.id, function(err, catr){
+        if(err){
+            console.log(err);
+            console.log("Testing");
+        } else {
+            console.log("Listing all venues....")
+            //render show template with that catering
+            res.render("catering/showcatering", {catering: catr});
+        }
+    });
+});
+
+
+//decorations 
+router.get("/decorations/:id", function(req, res){
+    //find the decorations with provided ID
+    Decorations.findById(req.params.id, function(err, deco){
+        if(err){
+            console.log(err);
+            console.log("Testing");
+        } else {
+            console.log("Listing all venues....")
+            //render show template with that decorations
+            res.render("decoration/showdecoration", {decorations: deco});
+        }
+    });
+});
+
+router.get("/newVenue", function(req, res){
+    res.render("venues/new"); 
+});
+
+router.get("/newDecoration", function(req, res){
+    res.render("decoration/new"); 
+});
+
+router.get("/newCaterer", function(req, res){
+    res.render("catering/new"); 
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    req.flash("error", "You must be signed in to do that!");
+    res.redirect("/login");
+}
+
+//CREATE - add new venue to DB
+router.post("/venues", isLoggedIn, function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var price = req.body.price;
+    var loc = req.body.location;
+    var cap = req.body.capacity;
+    var cat = req.body.category;
+    var con = req.body.contactno;
+    var cater = req.body.cateringAvailable;
+    var dec = req.body.decorationAvailable;
+    var flag = false;
+    if(name == "" || image == "" || desc == "" || price == "" || loc == "" || cap == "" || cat == "" || cater == "" || dec == "" || con == "") {
+        console.log("Venue info not complete.");
+        req.flash("error", "There cannot be an empty field!!");
+        res.redirect("/newVenue");
+    } else {
+        flag = true;
+        var newVenue = {name: name, image: image, description: desc, price: price, location: loc, capacity: cap, 
+            category: cat, contactno: con, cateringAvailable: cater, decorationAvailable: dec}
+        // Create a new venue and save to DB
+        if(flag) {
+            Venues.create(newVenue, function(err, newlyCreated){
+                if(err){
+                    console.log(err);
+                } else {
+                    //redirect back to venues page
+                    console.log(newlyCreated);
+                    res.redirect("/venues");
+                }
+            });
+        }
+    }
+
+});
+
+//CREATE - add new Decorator to DB
+router.post("/decorations", isLoggedIn, function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var price = req.body.price;
+    var loc = req.body.location;
+    var con = req.body.contactno;
+    var flag = false;
+    if(name == "" || image == "" || desc == "" || price == "" || loc == "" || con == "") {
+        console.log("Decorator info not complete.");
+        req.flash("error", "There cannot be an empty field!!");
+        res.redirect("/newDecoration");
+    } else {
+        flag = true;
+        var newDeco = {name: name, image: image, description: desc, price: price, location: loc, 
+            contactno: con}
+        // Create a new Decorator and save to DB
+        if(flag) {
+            Decorations.create(newDeco, function(err, newlyCreated){
+                if(err){
+                    console.log(err);
+                } else {
+                    //redirect back to venues page
+                    console.log(newlyCreated);
+                    res.redirect("/decorations");
+                }
+            });
+        }
+    }
+
+});
+
+
+//CREATE - add new Caterer to DB
+router.post("/catering", isLoggedIn, function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var price = req.body.price;
+    var loc = req.body.location;
+    var con = req.body.contactno;
+    var bev = req.body.beverages;
+    var flag = false;
+    if(name == "" || image == "" || desc == "" || price == "" || loc == "" || con == "" || bev == "") {
+        console.log("Caterer info not complete.");
+        req.flash("error", "There cannot be an empty field!!");
+        res.redirect("/newCaterer");
+    } else {
+        flag = true;
+        var newCaterer = {name: name, image: image, description: desc, price: price, location: loc, 
+            contactno: con, beverages: bev}
+        // Create a new Caterer and save to DB
+        if(flag) {
+            Catering.create(newCaterer, function(err, newlyCreated){
+                if(err){
+                    console.log(err);
+                } else {
+                    //redirect back to venues page
+                    console.log(newlyCreated);
+                    res.redirect("/catering");
+                }
+            });
+        }
+    }
+
+});
+
 
 
 module.exports = router;
