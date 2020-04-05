e988686cb82a31f0231d17c48d5a8f92fcfaa667
Amanpreet Singh <55251373+a493sing@users.noreply.github.com>
2020-04-04 22:23:28 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -27,14 +27,17 @@ router.post("/register", function(req, res){
     var newUser = new User({username: req.body.username});
     User.register(newUser, req.body.password, function(err, user){
         if(err){
-            console.log(err);
-            req.flash("error", err.message);
-            return res.render("register");
+           console.log(err.message);
+	failureFlash: true
+           req.flash("error", err.message);
+	res.redirect("/register");
         }
+	else {
         passport.authenticate("local")(req, res, function(){
            req.flash("success", "Successfully Signed Up! Start your planning now " + req.body.username);
            res.redirect("/"); 
         });
+	}
     });
 });
 
@@ -48,8 +51,9 @@ router.get("/login", function(req, res){
 //handling login logic
 router.post("/login", passport.authenticate("local", 
     {
-        successRedirect: "/venues",
-        failureRedirect: "/login"
+        successRedirect: "/",
+        failureRedirect: "/login",
+	failureFlash: true
     }), function(req, res){
 });
 
@@ -61,12 +65,52 @@ router.get("/logout", function(req, res){
    res.redirect("/");
 });
 
-// function isLoggedIn(req, res, next){
-//     if(req.isAuthenticated()){
-//         return next();
-//     }
-//     req.flash("error", "You must be signed in to do that!");
-//     res.redirect("/login");
-// }
+
+router.get("/venues/:id", function(req, res){
+    //find the venues with provided ID
+    Venues.findById(req.params.id, function(err, ven){
+        if(err){
+            console.log(err);
+            console.log("Testing");
+        } else {
+            console.log("Listing all venues....")
+
+            //render show template with that venues
+            res.render("venues/showvenue", {venue: ven});
+        }
+    });
+});
+
+
+//catering
+router.get("/catering/:id", function(req, res){
+    //find the catering with provided ID
+    Catering.findById(req.params.id, function(err, catr){
+        if(err){
+            console.log(err);
+            console.log("Testing");
+        } else {
+            console.log("Listing all venues....")
+            //render show template with that catering
+            res.render("catering/showcatering", {catering: catr});
+        }
+    });
+});
+
+
+//decorations 
+router.get("/decorations/:id", function(req, res){
+    //find the decorations with provided ID
+    Decorations.findById(req.params.id, function(err, deco){
+        if(err){
+            console.log(err);
+            console.log("Testing");
+        } else {
+            console.log("Listing all venues....")
+            //render show template with that decorations
+            res.render("decoration/showdecoration", {decorations: deco});
+        }
+    });
+});
 
 module.exports = router;
