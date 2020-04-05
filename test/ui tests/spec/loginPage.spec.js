ef59de35d17f39d6afc57737be5eaa168112202b
Amanpreet Singh <55251373+a493sing@users.noreply.github.com>
2020-04-05 15:00:26 -0400

    Add files via upload

--- test/ui tests/spec/loginPage.spec.js
+++ test/ui tests/spec/loginPage.spec.js
@@ -17,28 +17,77 @@ jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000; // in microseconds.
             })
 
             afterEach(async function(){
-                let currentUrl = await page.getCurrentUrl();
-                // check whether it redirects to login page
-                expect(currentUrl.includes("login")).toBe(true);
                 await page.quit();
             })
 
             it("should not login if username and password is not provided",async function(){
+                let currentUrl = await page.getCurrentUrl();
+                console.log(currentUrl);
+                expect(currentUrl.includes("login")).toBe(true);
+
                 await loginButton.click();
+
+                currentUrl = await page.getCurrentUrl();
+                console.log(currentUrl);
+                expect(currentUrl.includes("login")).toBe(true);
                 console.log("\nLogin Page should not login if username and password is not provided")
             })
 
             it("should not login if only username is provided",async function(){
+                let currentUrl = await page.getCurrentUrl();
+                console.log(currentUrl);
+                expect(currentUrl.includes("login")).toBe(true);
+
                 await username.sendKeys("logan");
                 await loginButton.click();
+
+                currentUrl = await page.getCurrentUrl();
+                console.log(currentUrl);
+                expect(currentUrl.includes("login")).toBe(true);
                 console.log("\nLogin Page should not login if only username is provided")
             })
 
             it("should not login if only password is provided",async function(){
+                let currentUrl = await page.getCurrentUrl();
+                console.log(currentUrl);
+                expect(currentUrl.includes("login")).toBe(true);
+
                 await password.sendKeys("logan");
                 await loginButton.click();
+
+                currentUrl = await page.getCurrentUrl();
+                console.log(currentUrl);
+                expect(currentUrl.includes("login")).toBe(true);
+
                 console.log("\nLogin Page should not login if only password is provided")
             })
+
+            it("should go back to home", async function(){
+                let currentUrl = await page.getCurrentUrl();
+                console.log(currentUrl);
+                expect(currentUrl.includes("login")).toBe(true);
+
+                let goBackLink = await page.findByLinkText("Go Back")                
+                await goBackLink.click();
+
+                currentUrl = await page.getCurrentUrl();
+                console.log(currentUrl);
+                expect(currentUrl.includes("login")).toBe(false);
+            })
+
+            it("should redirect to register page",async function(){
+                let currentUrl = await page.getCurrentUrl();
+                console.log(currentUrl);
+                expect(currentUrl.includes("login")).toBe(true);
+
+                let registerLink = await page.findByLinkText("Register")
+                await registerLink.click();
+                
+                currentUrl = await page.getCurrentUrl();
+                console.log(currentUrl);
+                expect(currentUrl.includes("register")).toBe(true);
+            })
+
         })
     } catch (error) {
         console.log(error)
